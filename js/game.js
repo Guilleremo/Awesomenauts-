79083cde8caa5a79ee59aa74ccafb19f3196c622
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-02-08 22:51:05 -0800

    finished all of the videos

--- js/game.js
+++ js/game.js
@@ -39,12 +39,12 @@ var game = {
 
 	// Run on game resources loaded.
 	"loaded" : function () {
-                me.pool.register("player", game.PlayerEntity, true);
-                me.pool.register("EnemyBase", game.EnemyBaseEntity);
-                me.pool.register("PlayerBase", game.PlayerBaseEntity);
-                me.pool.register("creep1", game.EnemyCreep, true);
-                me.pool.register("GameManager", game.GameManager);
-            
+		me.pool.register("player", game.PlayerEntity, true);
+		me.pool.register("PlayerBase", game.PlayerBaseEntity);
+		me.pool.register("EnemyBase", game.EnemyBaseEntity);
+		me.pool.register("creep1", game.EnemyCreep, true);
+		me.pool.register("GameManager", game.GameManager);
+
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
--- js/screens/play.js
+++ js/screens/play.js
@@ -5,14 +5,22 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
-		me.levelDirector.loadLevel("level01");
-
-		var player = me.pool.pull("player", 0, 420, {});
-		me.game.world.addChild(player, 5);
-
-		me.input.bindKey(me.input.KEY.RIGHT, "right");
-		me.input.bindKey(me.input.KEY.A, "attack");
-
+                
+                me.levelDirector.loadLevel("level01");
+                //you are loading your level on the webpage 
+                
+                var player = me.pool.pull("player", 0, 0, {});
+                me.game.world.addChild(player, 5);
+                //created a character to pop on the screen
+                
+                var gamemanager = me.pool.pull("GameManager", 0, 0, {});
+                me.game.world.addChild(gamemanager, 0);
+                
+                me.input.bindKey(me.input.KEY.RIGHT, "right");
+                me.input.bindKey(me.input.KEY.LEFT, "left");
+                me.input.bindKey(me.input.KEY.SPACE, "jump");
+                me.input.bindKey(me.input.KEY.A, "attack");
+                
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
