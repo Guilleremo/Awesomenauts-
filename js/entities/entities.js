5a850638436cbd51834074279255b82157e8a6f2
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-02-12 12:18:34 -0800

    finished video 22 coded and commented

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,19 +10,21 @@ game.PlayerEntity = me.Entity.extend({
                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
             }
         }]);
-       this.body.setVelocity(5, 20);
-       //keeps track of which direction your character is going
-       this.facing = "right";
-       this.now = new Date().getTime();
-       this.lastHit = this.now;
-       this.lastAttack = new Date().getTime();  //Haven't used this
-       me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
-       
-       this.renderable.addAnimation("idle", [78]);
-       this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
-       this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
-       
-       this.renderable.setCurrentAnimation("idle");
+		this.type = "PlayerEntity";
+		this.health = 20;
+		this.body.setVelocity(5, 20);
+		//keeps track of which direction your character is going
+		this.facing = "right";
+		this.now = new Date().getTime();
+		this.lastHit = this.now;
+		this.lastAttack = new Date().getTime();  //Haven't used this
+		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+        
+		this.renderable.addAnimation("idle", [78]);
+		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+		this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
+        
+		this.renderable.setCurrentAnimation("idle");
     },
     
     update: function(delta){
@@ -77,6 +79,11 @@ game.PlayerEntity = me.Entity.extend({
         return true;
     },
     
+    loseHealth: function(damage){
+    	this.health = this.health - damage;
+    	console.log(this.health);
+    },
+
     collideHandler: function(response){
         if(response.b.type==='EnemyBaseEntity'){
             var ydif = this.pos.y - response.b.pos.y;
@@ -221,6 +228,8 @@ game.EnemyCreep = me.Entity.extend({
         this.renderable.setCurrentAnimation("walk");
     
     },
+
+    
     
     update: function(delta){
     	this.now = new Date().getTime();
@@ -242,7 +251,7 @@ game.EnemyCreep = me.Entity.extend({
     collideHandler: function(response){
     	if(response.b.type==='PlayerBase'){
     		this.attacking=true;
-    		this.lastAttacking=this.now;
+    		//this.lastAttacking=this.now;
     		this.body.vel.x = 0;
     		//keeps moving the creep to the right to maintain its position
     		this.pos.x = this.pos.x + 1;
@@ -254,8 +263,27 @@ game.EnemyCreep = me.Entity.extend({
     			//damage of 1
     			repsonse.b.loseHealth(1);
     		}
-    	} 
+    	}else if (response.b.type==='PlayerEntity'){
+    		var xdif = this.pos.x = response.b.pos.x;
+
+    		this.attacking=true;
+    		//this.lastAttacking=this.now;
+    		if(xdif>0){
+    			this.pos.x = this.pos.x + 1;
+    			this.body.vel.x = 0;
+    			//keeps moving the creep to the right to maintain its position
+    		}
+    		//checks that is has been at least 1 second since this creep hit something
+    		if((this.now-this.lastHit >= 1000) && xdif>0){
+    			//updates the lastHit timer
+    			this.lastHit = this.now;
+    			//makes the player call its longHealth function and passes it a 
+    			//damage of 1
+    			repsonse.b.loseHealth(1);
+    	}
     }
+
+   }
 });
 
 game.GameManager = Object.extend({
