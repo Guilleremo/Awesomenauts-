10d7345f53e7351c3a9f07b2d9e47c6dc56e03ca
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-02-27 19:59:45 -0800

    finished video 33

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -132,7 +132,14 @@ game.PlayerEntity = me.Entity.extend({
 
     collideHandler: function(response){
         if(response.b.type==='EnemyBaseEntity'){
-            var ydif = this.pos.y - response.b.pos.y;
+            this.collideWithEnemyBase();
+        }else if(response.b.type==='EnemyCreep'){
+        	this.collideWithEnemyCreep(response);	
+        }
+    },
+
+    collideWithEnemyBase(){
+    		var ydif = this.pos.y - response.b.pos.y;
             var xdif = this.pos.x - response.b.pos.x;
             
             if(ydif<-40 && xdif< 70 && xdif>-35){
@@ -141,45 +148,60 @@ game.PlayerEntity = me.Entity.extend({
             }
             else if(xdif>-35 && this.facing==='right' && (xdif<0)){
                 this.body.vel.x = 0;
-                //this.pos.x = this.pos.x -1;
             }else if(xdif<70 && this.facing==='left' && (xdif>0)){
                 this.body.vel.x = 0;
-                //this.pos.x = this.pos.x +1;
             }
-            
             if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer){
                 this.lastHit = this.now;
                 response.b.loseHealth(game.data.playerAttack);
             }
-        }else if(response.b.type==='EnemyCreep'){
-        	var xdif = this.pos.x - response.b.pos.x;
+    },
+
+    collideWithEnemyCreep: function(){
+    		var xdif = this.pos.x - response.b.pos.x;
         	var ydif = this.pos.x - response.b.pos.x;
 
-        	if(xdif>0){
-        		//this.pos.x = this.pos.x + 1;
+        	this.stopMovement(xdif);
+
+        	if(this.checkAttack(xdif, ydif){
+			this.hitCreep(response);
+        	};
+    },
+
+    stopMovement: function(xdif){
+    		if(xdif>0){
         		if(this.facing==="left"){
         			this.body.vel.x = 0;
         		}
         	}else{
-        		//this.pos.x = this.pos.x + 1;
         		if(this.facing==="right"){
         			this.body.vel.x = 0;
         		}
         	}
-        	if(this.renderable.isCurrentAnimation("attack")&& this.now-this.lastHit >= game.data.playerAttackTimerk
+    },
+
+    checkAttack: function(xdif, ydif, response){
+    		if(this.renderable.isCurrentAnimation("attack")&& this.now-this.lastHit >= game.data.playerAttackTimerk
         			&& (Math.abs(ydif) <=40) && 
         			(((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
         			){
         		this.last = this.now;
-        	//if the creeps health is less than our attack, execute code in if statement
+        	return true;
+        }
+        return false;
+    },
+
+    hitCreep: function(){
+    		//if the creeps health is less than our attack, execute code in if statement
         	if(response.b.health <= this.attack){
+
         		game.data.gold += 1;
         		console.log("Current gold; " + game.data.gold)
         	}
 
         		response.b.loseHealth(game.data.playerAttack);
         	}
-        }
     }
+
 });
 
