bc6ac5ebb8587dd79e7db326fbb223bc9ec951ea
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-03-06 12:00:00 -0800

    finished video 40 all done

--- js/screens/title.js
+++ js/screens/title.js
@@ -31,6 +31,27 @@ game.TitleScreen = me.ScreenObject.extend({
 			}
 		})));
 
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [380, 340, 280,50]);
+				this.font = new me.Font("Ariel", 46, "white");
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+			},
+
+			draw: function(render){
+				this.font.draw(render.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt){
+				return true;
+			},
+
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.state.change(me.state.PLAY);
+			}
+		})));
+
 	},
 	
 	
