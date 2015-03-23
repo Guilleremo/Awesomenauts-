b5e921eaf9de06191b667ffa11d6f6d3305c1467
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-03-23 13:10:52 -0700

    finished video 47 all done and it all works

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -16,7 +16,7 @@ game.GameTimerManager = Object.extend({
 
     goldTimerCheck: function(){
         if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
-            game.data.gold += 1;
+            game.data.gold += (game.data.exp1+1);
             console.log("Current gold: " + game.data.gold);
         }
     },
@@ -69,5 +69,67 @@ game.ExperienceManager = Object.extend({
         }
         this.gameover = true;
         me.seve.exp = game.data.exp;
+    }
+});
+
+game.ExperienceManager = Object.extend({
+    init function(x, y, settings){
+        this.now = new Date().getTime();
+        this.lastBuy = new Date().getTime();
+        this.paused = false;
+        this.alwaysUpdate = true;
+        this.updateWhenPaused = true;
+    },
+    update: function(){
+        this.now = new Date().getTime();
+
+        if(me.input.isKeyPressed("buy") && this.now-this.lastBuy >= 1000){
+            this.lastBuy = this.now;
+            if(!this.buying){
+                this.startBuying();
+            }else{
+                this.stopBuying;
+            }
+
+        }
+
+    
+        return true;
+    },
+
+    startBuying: function(){
+        this.buying = true;
+        me.state.pause(me.state.PLAY);
+        game.data.pausePos = me.game.localToWorld(0, 0);
+        game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+        game.data.buyscreen.updateWhenPaused = true;
+        game.data.buyscreen.setOpacity(0.8);
+        me.game.world.addChild(game.data.buyScreen, 34);
+        game.data.player.body.setVelocity(0, 0);
+        me.state.pause(me.state,PLAY);
+        me.input.bindKey(me.input.KEY.F1, "F1", true);
+        me.input.bindKey(me.input.KEY.F2, "F2", true);
+        me.input.bindKey(me.input.KEY.F3, "F3", true);
+        me.input.bindKey(me.input.KEY.F4, "F4", true);
+        me.input.bindKey(me.input.KEY.F5, "F5", true);
+        me.input.bindKey(me.input.KEY.F6, "F6", true);
+        this.setBuyText();
+    },
+
+    setBuyText: function(){
+        
+    },
+
+    stopBuying: function(){
+        this.buying = false;
+        me.state.resume(me.state.PLAY);
+        game.data.player.body.setVelocity(game.data.playerMoveSeed, 20);
+        me.game.world.removeChild(game.data.buyscreen);
+        me.input.unbindKey(me.input.KEY.F1, "F1", true);
+        me.input.unbindKey(me.input.KEY.F2, "F2", true);
+        me.input.unbindKey(me.input.KEY.F3, "F3", true);
+        me.input.unbindKey(me.input.KEY.F4, "F4", true);
+        me.input.unbindKey(me.input.KEY.F5, "F5", true);
+        me.input.unbindKey(me.input.KEY.F6, "F6", true);
     }
 });
