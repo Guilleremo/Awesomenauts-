aceb4dff9b7fdb69102f3a7eece37e8cc1c23c9b
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-03-25 11:54:23 -0700

    videos 48 and 49 are all finished

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -117,7 +117,24 @@ game.ExperienceManager = Object.extend({
     },
 
     setBuyText: function(){
-        
+        me.game.world.addChild(new (me.Renderable.extend({
+            init: function(){
+                this._super(me.Renderable, 'init', [game.data.pausePos.x, game.data.pausePos.y,300,50]);
+                this.font = new me.Font("Ariel", 26, "white");
+                this.updateWhenPaused = true;
+                this.alwaysUpdate = true;
+            },
+
+            draw: function(renderer){
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT. Current Gold:", + game.data.gold, this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "Skill 1: Increase Damage. Current Level: ", + game.data.skill1 +" Cost: " ((game.data.exp1+1)*10),this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "Skill 2: Run Faster! Current Level: ",  + game.data.skill2 +" Cost: " ((game.data.exp2+1)*10),this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "Skill 3: Increase Health. Current Level: " + game.data.skill3 +" Cost: " ((game.data.exp3+1)*10), this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "Q Ability: Speed Burst: Current Level: ", + game.data.ability1 +" Cost: " ((game.data.exp4+1)*10), this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "W Ability: Eat Your Creep For Health: ", + game.data.ability2 +" Cost: " ((game.data.exp5+1)*10), this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "E Ability: Throw Your Spear: ", + game.data.ability3 +" Cost: " ((game.data.exp6+1)*10), this.pos.x, this.pos.y);
+        }));
+        me.game.world.addChild(game.datat.nuyText, 35);
     },
 
     stopBuying: function(){
@@ -131,5 +148,7 @@ game.ExperienceManager = Object.extend({
         me.input.unbindKey(me.input.KEY.F4, "F4", true);
         me.input.unbindKey(me.input.KEY.F5, "F5", true);
         me.input.unbindKey(me.input.KEY.F6, "F6", true);
+        me.game.world.removeChild(game.data.buyText);
+
     }
 });
